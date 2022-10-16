ccfa35344bdf25c7ed7ff8f24d22a3e31a8c0f82
Harry-KNIGHT <eliott.knight@icloud.com>
2022-10-16 11:36:12 +0200

    Add view model and method for add finished session to array

---
+++ BougeTesJambes/ViewModel/FinishedSessionViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  FinishedSessionViewModel.swift
+//  BougeTesJambes
+//
+//  Created by Elliot Knight on 16/10/2022.
+//
+
+import Foundation
+
+class FinishedSessionViewModel: ObservableObject {
+	@Published public var fishishedSessions: [SessionModel] = []
+
+	func addFinishedSession(sportType: Sport, timeObjectif: Int, ditanceObjectifInKm: Int, averageSpeedObjectif: Double, sessionTime: Int, sessionDistanceInKm: Int, sessionAverageSpeed: Double) {
+
+		let finishedSession = SessionModel(sportType: sportType, timeObjectif: timeObjectif, ditanceObjectifInKm: ditanceObjectifInKm, averageSpeedObjectif: averageSpeedObjectif, sessionTime: sessionTime, sessionDistanceInKm: sessionDistanceInKm, sessionAverageSpeed: sessionAverageSpeed)
+
+		self.fishishedSessions.append(finishedSession)
+	}
+}
