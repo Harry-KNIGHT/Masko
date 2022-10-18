e01909c47317bd527026b03dbbd3feaedbb122be
Harry-KNIGHT <eliott.knight@icloud.com>
2022-10-18 10:25:24 +0200

    Customise started session view colors.

--- BougeTesJambes/StartedSessionView.swift
+++ BougeTesJambes/StartedSessionView.swift
@@ -23,93 +23,102 @@ struct StartedSessionView: View {
 
 	@State private var pausedSession: Bool = false
 	var body: some View {
-		VStack {
-			List {
+		ZStack {
+			Color("viewBackgroundColor").ignoresSafeArea()
+			VStack {
+				Spacer()
+
 				SessionInformation(
 					sfSymbol: "stopwatch", objectif: "\(String(session.timeObjectif))min",
 					sessionValue: "\(String(convertTimeVM.convertSecInTime(timeInSeconds: sessionTimer)))"
 				)
 				.foregroundColor(convertTimeVM.compareConvertedTimeAndSessionTime(convertedSecInMin: session.timeObjectif, sessionTime: sessionTimer) == true ? .green : .primary)
-
-				if motionManager.isPedometerAvailable {
-					SessionInformation(
-						sfSymbol: "flag", objectif: "\(String(session.ditanceObjectifInKm))km",
-						sessionValue: "\(String(format: "%.2tf \(sessionDistanceInKm > 1_000 ? "km" : "m")", sessionDistanceInKm))"
-					)
+				Spacer()
+				HStack {
+					if motionManager.isPedometerAvailable {
+						SessionInformation(
+							sfSymbol: "flag", objectif: "\(String(session.ditanceObjectifInKm))km",
+							sessionValue: "\(String(format: "%.2tf \(sessionDistanceInKm > 1_000 ? "km" : "m")", sessionDistanceInKm))"
+						)
+					}
+					Spacer()
+					if let location = locationManager.userLocation {
+						SessionInformation(
+							sfSymbol: "speedometer", objectif: "\(String(session.averageSpeedObjectif))km/h",
+							sessionValue: "\(convertLocValueVM.convertMeterPerSecIntoKmHour(meterPerSec: location.speed))"
+						)
+					}
 				}
+				.padding(.horizontal)
+				Spacer()
+				Button(action: {
+					pausedSession = true
+				}, label: {
+					ZStack(alignment: .center) {
+						Circle()
+							.fill(Color("actionInteractionColor").gradient)
+							.frame(height: 120)
+							.shadow(color: Color("actionInteractionColor"), radius: 10)
 
-				if let location = locationManager.userLocation {
-					SessionInformation(
-						sfSymbol: "speedometer", objectif: "\(String(session.averageSpeedObjectif))km/h",
-						sessionValue: "\(convertLocValueVM.convertMeterPerSecIntoKmHour(meterPerSec: location.speed))"
-					)
+						Image(systemName: pausedSession ? "play.fill" : "pause.fill")
+							.font(.custom("",size: 60, relativeTo: .largeTitle))
+							.foregroundColor(.white)
+					}
+				})
+				.alert("Arrêter la session ?", isPresented: $pausedSession) {
+					Button("Oui", role: .destructive) {
+						path.removeLast()
+
+						self.finishedSesionVM.fishishedSessions.append(SessionModel(sportType: session.sportType, timeObjectif: session.timeObjectif, ditanceObjectifInKm: session.ditanceObjectifInKm, averageSpeedObjectif: session.averageSpeedObjectif, sessionTime: sessionTimer, sessionDistanceInKm: sessionDistanceInKm, sessionAverageSpeed: sessionAverageSpeed))
+					}
+					Button("Non", role: .cancel) {
+						pausedSession = false
+					}
 				}
 			}
 
-			Button(action: {
-				pausedSession = true
-			}, label: {
-				ZStack(alignment: .center) {
-					Circle()
-						.fill(Color(.blue).gradient)
-						.frame(height: 120)
-						.shadow(color: Color(.blue), radius: 10)
-
-					Image(systemName: pausedSession ? "play.fill" : "pause.fill")
-						.font(.custom("",size: 60, relativeTo: .largeTitle))
-						.foregroundColor(.white)
+			.onAppear {
+				motionManager.initializePodometer()
+			}
+			.onChange(of: motionManager.distance, perform:  { distance in
+				if let distance {
+					if distance > 0 {
+						sessionDistanceInKm = distance
+					}
 				}
 			})
-			.alert("Arrêter la session ?", isPresented: $pausedSession) {
-				Button("Oui", role: .destructive) {
-					path.removeLast()
+			.onChange(of: locationManager.userLocation, perform: {  location in
+				if let location {
+					if location.speed > 0 {
+						sessionAverageSpeed = location.speed
+					}
+				}
+			})
 
-					self.finishedSesionVM.fishishedSessions.append(SessionModel(sportType: session.sportType, timeObjectif: session.timeObjectif, ditanceObjectifInKm: session.ditanceObjectifInKm, averageSpeedObjectif: session.averageSpeedObjectif, sessionTime: sessionTimer, sessionDistanceInKm: sessionDistanceInKm, sessionAverageSpeed: sessionAverageSpeed))
-				}
-				Button("Non", role: .cancel) {
-					pausedSession = false
-				}
-			}
-		}
-		.onAppear {
-			motionManager.initializePodometer()
-		}
-		.onChange(of: motionManager.distance, perform:  { distance in
-			if let distance {
-				if distance > 0 {
+			.onChange(of: motionManager.distance, perform: { distance in
+				if let distance = motionManager.distance {
 					sessionDistanceInKm = distance
 				}
-			}
-		})
-		.onChange(of: locationManager.userLocation, perform: {  location in
-			if let location {
-				if location.speed > 0 {
-					sessionAverageSpeed = location.speed
+			})
+			.onReceive(timer) { _ in
+				sessionTimer += 1
+
+				if convertTimeVM.isSessionTimeBiggerThanConvertedTime(sessionTime: sessionTimer, convertedSecInMin: session.timeObjectif) {
+					playSongVM.playsong(sound: "mixkit-arcade", type: "wav")
 				}
 			}
-		})
+			.navigationBarBackButtonHidden(true)
+			.navigationBarTitleDisplayMode(.inline)
+			.toolbar {
+				ToolbarItem(placement: .principal) {
+					HStack {
+						session.sportType.sportIcon
+						Text(session.sportType.sportName)
 
-		.onChange(of: motionManager.distance, perform: { distance in
-			if let distance = motionManager.distance {
-				sessionDistanceInKm = distance
-			}
-		})
-		.onReceive(timer) { _ in
-			sessionTimer += 1
-
-			if convertTimeVM.isSessionTimeBiggerThanConvertedTime(sessionTime: sessionTimer, convertedSecInMin: session.timeObjectif) {
-				playSongVM.playsong(sound: "mixkit-arcade", type: "wav")
-			}
-		}
-		.navigationBarBackButtonHidden(true)
-		.navigationBarTitleDisplayMode(.inline)
-		.toolbar {
-			ToolbarItem(placement: .principal) {
-				HStack {
-					session.sportType.sportIcon
-					Text(session.sportType.sportName)
+					}
+					.foregroundColor(.white)
+					.font(.title2.bold())
 				}
-				.font(.title2.bold())
 			}
 		}
 	}
@@ -133,16 +142,16 @@ struct SessionInformation: View {
 	var sessionValue: String
 
 	var body: some View {
-		VStack(alignment: .leading, spacing: 10) {
-			HStack {
-				Image(systemName: sfSymbol)
-				Text("\(objectif)")
-			}
-			.font(.title3)
-			.foregroundColor(.secondary)
+		VStack(alignment: .center, spacing: 10) {
+			Image(systemName: sfSymbol)
+				.font(.title)
 
 			Text("\(sessionValue)")
-				.font(.title2)
-		}
+				.font(.largeTitle.bold())
+
+			Text("\(objectif)")
+				.opacity(0.5)
+				.font(.title3)
+		}.foregroundColor(.white)
 	}
 }
