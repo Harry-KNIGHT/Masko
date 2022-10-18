73db0e7f2568853c9021c9b7fcd6e0213ba1a50d
Harry-KNIGHT <eliott.knight@icloud.com>
2022-10-18 15:35:48 +0200

    Modify chart style.

--- BougeTesJambes/FinishedSessionDetailView.swift
+++ BougeTesJambes/FinishedSessionDetailView.swift
@@ -31,16 +31,49 @@ struct FinishedSessionDetailView: View {
 					objectifType: "Vitesse",
 					sessionInfo: "\(convertLocValueVM.convertMeterPerSecIntoKmHour(meterPerSec: session.sessionAverageSpeed))",
 					objectif: "\(session.averageSpeedObjectif)km/h")
+				
+					if let distanceSpeedChart = session.distanceSpeedChart {
+						Chart(distanceSpeedChart) { value in
+							LineMark(
+								x: .value("distance", value.sessionDistance),
+								y: .value("vitesse", value.averageSpeed)
+							)
+							.foregroundStyle(Color("textActionColor"))
 
-				if let distanceSpeedChart = session.distanceSpeedChart {
-					Chart(distanceSpeedChart) { value in
-						LineMark(
-							x: .value("distance", value.sessionDistance),
-							y: .value("vitesse", value.averageSpeed)
-						)
+						}
+						.chartYScale(domain: .automatic(includesZero: false))
+						.chartXAxis {
+							AxisMarks(values: .automatic) { value in
+								AxisGridLine(centered: true, stroke: StrokeStyle(dash: [1, 2]))
+									.foregroundStyle(Color.cyan)
+								AxisTick(centered: true, stroke: StrokeStyle(lineWidth: 2))
+									.foregroundStyle(Color.red)
+								AxisValueLabel() {
+									if let intValue = value.as(Int.self) {
+										Text("\(intValue) \(intValue > 1_000 ? "km" : "m")")
+											.font(.system(size: 10)) // style it
+											.foregroundColor(.white)
+									}
+								}
+							}
+						}
+						.chartYAxis {
+							AxisMarks(values: .automatic) { value in
+								AxisGridLine(centered: true, stroke: StrokeStyle(dash: [1, 2]))
+									.foregroundStyle(Color.cyan)
+								AxisTick(centered: true, stroke: StrokeStyle(lineWidth: 2))
+									.foregroundStyle(Color.red)
+								AxisValueLabel() { // construct Text here
+									if let intValue = value.as(Int.self) {
+										Text("\(intValue)km/h")
+											.font(.system(size: 10)) // style it
+											.foregroundColor(.white)
+									}
+								}
+							}
+						}
+						.frame(height: 250)
 					}
-					.frame(height: 250)
-				}
 				Spacer()
 
 			}
