cdcf2f79684608a8947028a12edb350ab9ed5405
Harry-KNIGHT <eliott.knight@icloud.com>
2022-10-18 09:48:02 +0200

    Change shadow radius and opacity.

--- BougeTesJambes/SelectSportButtonsView.swift
+++ BougeTesJambes/SelectSportButtonsView.swift
@@ -19,7 +19,7 @@ struct SelectSportButtonsView: View {
 						RoundedRectangle(cornerRadius: 10, style: .circular)
 							.frame(height: 50)
 							.foregroundColor(choice == sportChoosen ? .blue : .gray)
-							
+							.shadow(color: choice == sportChoosen ? .blue : .white.opacity(0),  radius: 7)
 						Text(choice.sportName)
 							.foregroundColor(.white)
 							.font(.title3.bold())
