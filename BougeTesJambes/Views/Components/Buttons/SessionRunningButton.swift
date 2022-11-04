53a9cf867d6cabf78a6ed0e87326560685a94401
Harry-KNIGHT <eliott.knight@icloud.com>
2022-11-04 21:12:48 +0100

    Animate button from tall to small

--- BougeTesJambes/Views/Components/Buttons/SessionRunningButton.swift
+++ BougeTesJambes/Views/Components/Buttons/SessionRunningButton.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SessionRunningButton: View {
 	@Binding var isSessionPaused: Bool
-
+	@State private var buttonWidth = 2.0
 	var body: some View {
 		ZStack {
 			Circle()
@@ -20,6 +20,12 @@ struct SessionRunningButton: View {
 			Image(systemName: isSessionPaused ? "play.fill" : "pause.fill")
 				.font(.custom("", size: 60, relativeTo: .largeTitle))
 				.foregroundColor(.white)
+		}
+		.scaleEffect(buttonWidth)
+		.onAppear {
+			withAnimation(.easeOut(duration: 0.7)) {
+				buttonWidth = 1
+			}
 		}
 		.onTapGesture {
 			isSessionPaused = true
