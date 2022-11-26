2e10753dcef96d764105636639da191eea9ee80d
Harry-KNIGHT <eliott.knight@icloud.com>
2022-11-26 12:34:34 +0100

    Fix Nan value and conversion from double to Int

--- BougeTesJambes/ViewModel/CalculPaceViewModel.swift
+++ BougeTesJambes/ViewModel/CalculPaceViewModel.swift
@@ -10,18 +10,15 @@ import Foundation
 class CalculPaceViewModel: ObservableObject {
 
 	func calculPace(startedSessionEpoch: Double, nowEpoch: Double, meters: Double) -> String {
+		guard meters >= 1_000  else { return "00:00" }
 
-		let distance: Double = (meters > 999 ? meters / 1_000 : meters)
+		let distance: Double = (meters >= 1_000 ? meters / 1_000 : meters)
 		let sessionTime = (nowEpoch - startedSessionEpoch)
-		let paceInSec = (sessionTime / distance)
+		let paceInSec = Int(sessionTime / distance)
 
-		let min = (Int(paceInSec) / 60)
-		let sec = (Int(paceInSec) % 60)
+		let min = (paceInSec / 60)
+		let sec = (paceInSec % 60)
 
-		if meters > 1_000 {
-			return (String(format: "%02i:%02i min/km", min, sec))
-		} else {
-			return "00:00"
-		}
+		return (String(format: "%02i:%02i", min, sec))
 	}
 }
